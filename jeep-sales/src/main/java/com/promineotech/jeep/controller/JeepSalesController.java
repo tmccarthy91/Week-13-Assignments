5442a3deeeb3d1a864e83155aa2d3479de2557c5
Timothy "Timm" McCarthy <121832495+tmccarthy91@users.noreply.github.com>
2023-04-16 20:05:20 -0600

    Add files via upload

--- jeep-sales/src/main/java/com/promineotech/jeep/controller/JeepSalesController.java
+++ jeep-sales/src/main/java/com/promineotech/jeep/controller/JeepSalesController.java
@@ -2,11 +2,17 @@ package com.promineotech.jeep.controller;
 
 import java.util.List;
 
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.promineotech.jeep.Constants;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
@@ -19,12 +25,14 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
+@Validated
 @RequestMapping("/jeeps")
 @OpenAPIDefinition(info = @Info(title = "Jeep Sales Service"), servers = {
 		@Server(url = "http://localhost:8080", description = "Local server.")})
 public interface JeepSalesController {
 	// @formatter:off
 	
+
 	@Operation(
 		summary = "Returns a list of Jeeps",
 		description = "Returns a list of Jeeps given an optional model and/or trim",
@@ -66,7 +74,9 @@ public interface JeepSalesController {
 	@ResponseStatus(code = HttpStatus.OK)
 	List<Jeep> fetchJeeps(
 			@RequestParam(required = false) 
-				JeepModel model, 
+				JeepModel model,
+			@Length(max = Constants.TRIM_MAX_LENGTH)
+			@Pattern(regexp = "[\\w\\s]*")
 			@RequestParam(required = false)
 				String trim);
 	// @formatter:on
--- jeep-sales/src/main/java/com/promineotech/jeep/entity/Jeep.java
+++ jeep-sales/src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -1,6 +1,9 @@
 package com.promineotech.jeep.entity;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,7 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class Jeep {
+public class Jeep implements Comparable<Jeep>{
 	private Long modelPK;
 	private JeepModel modelId;
 	private String trimLevel;
@@ -20,4 +23,20 @@ public class Jeep {
 	private int wheelSize;
 	private BigDecimal basePrice;
 	
+	@JsonIgnore
+	public Long getModelPK() {
+		return modelPK;
+	}
+
+	@Override
+	public int compareTo(Jeep that) {
+		// @formatter:off
+		return Comparator
+				.comparing(Jeep::getModelId)
+				.thenComparing(Jeep::getTrimLevel)
+				.thenComparing(Jeep::getNumDoors)
+				.compare(this, that);
+		
+		// @formatter:on
+	}
 }
